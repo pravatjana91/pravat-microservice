36c7f67c6698cb10f686e6c0c21e84ef5128a7a7
pravatjana91 <pravatjana121@gmail.com>
2022-06-29 20:15:35 +0530

    log

--- departmentservice/src/main/java/com/pravat/userservice/controller/UserController.java
+++ departmentservice/src/main/java/com/pravat/userservice/controller/UserController.java
@@ -6,10 +6,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "v1/userservice")
+//@RequestMapping(value = "v1/userservice")
 public class UserController {
 
-	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String get() {
+
+		return "hello world departmet service ";
+	}
+	
+	@RequestMapping(value = "v1/userservice/{name}", method = RequestMethod.GET)
 	public String getLicenses(@PathVariable("name") String name) {
 
 		return "hello world user service " + name;
